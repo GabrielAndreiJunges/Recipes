f7634270ced291013a9314d667ea5d6082aa6815
Gabriel Andrei Junges <gabriel.junges@aprtech.com.br>
2025-05-30 08:04:30 -0300

    Criada página de receitas por categoria, seu view e template.

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,4 @@
+from turtle import title
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -6,6 +7,9 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
@@ -24,6 +28,10 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True)
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True
+        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None,
     )
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    cover = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
+
+    def __str__(self):
+        return self.title
