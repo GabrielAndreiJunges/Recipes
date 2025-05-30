6144c544e622bd68c5682a6696dc47922db817f4
Gabriel Andrei Junges <gabriel.junges@aprtech.com.br>
2025-05-30 09:21:18 -0300

    Alterado CSS da imagem das receitas.

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from utils.recipes.factory import make_recipe
+from django.shortcuts import render, get_list_or_404
 from .models import Recipe
 
 # Create your views here.
@@ -13,11 +12,12 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id
-    ).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe, category__id=category_id, is_published=True)
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
 
