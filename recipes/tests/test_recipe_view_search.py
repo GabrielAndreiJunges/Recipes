500f823e81293efa4f30bfb8377e55c2717868ca
Gabriel Andrei Junges <gabriel.junges@aprtech.com.br>
2025-06-04 16:11:39 -0300

    Criados parâmetros de seguração e ordenação de search.

--- recipes/tests/test_recipe_view_search.py
+++ recipes/tests/test_recipe_view_search.py
@@ -31,3 +31,26 @@ class RecipeViewsTest(RecipeTestBase):
             'Search for &quot;&lt;Teste&gt;&quot;',
             response.content.decode('utf-8')
         )
+
+    def test_recipe_search_can_find_recipe_by_title(self):
+        title1 = 'This is recipe one'
+        title2 = 'This is recipe two'
+
+        recipe1 = self.make_recipe(
+            slug='recipe-one',
+            title=title1,
+            author_data={'username': 'one'},
+        )
+
+        recipe2 = self.make_recipe(
+            slug='recipe-two',
+            title=title2,
+            author_data={'username': 'two'},
+        )
+        search_url = reverse('recipes:search')
+        response1 = self.client.get(f'{search_url}?q={title1}')
+        response2 = self.client.get(f'{search_url}?q={title2}')
+        responseboth = self.client.get(f'{search_url}?q=this')
+
+        self.assertIn(recipe1, response1.context['recipes'])
+        self.assertIn(recipe2, response2.context['recipes'])
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from django.db.models import Q
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Recipe
 
@@ -37,6 +38,16 @@ def search(request):
     if not search_term:
         raise Http404()
 
+    recipes = Recipe.objects.filter(
+        Q(Q(title__icontains=search_term) |
+          Q(description__icontains=search_term))
+    )
+
+    recipes = recipes.filter(is_published=True)
+    recipes = recipes.order_by('-id')
+
     return render(request, 'recipes/pages/search.html', {
-        'page_title': f'Search for "{search_term}"'
+        'page_title': f'Search for "{search_term}"',
+        'search_term': search_term,
+        'recipes': recipes,
     })
