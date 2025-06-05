67457baff72ae556eb8432c18dc86612d9412ae0
Gabriel Andrei Junges <gabriel.junges@aprtech.com.br>
2025-06-05 15:56:01 -0300

    Criada paginação para home page.

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,9 @@
+from django.core.paginator import Paginator
 from django.http import Http404
 from django.db.models import Q
 from django.shortcuts import render, get_list_or_404, get_object_or_404
+
+from utils.recipes.pagination import make_pagination_range
 from .models import Recipe
 
 # Create your views here.
@@ -8,8 +11,20 @@ from .models import Recipe
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+    paginator = Paginator(recipes, 3)
+    page_object = paginator.get_page(current_page)
+
+    pagination_range = make_pagination_range(
+        paginator.page_range, 4, current_page)
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_object,
+        'pagination_range': pagination_range
     })
 
 
