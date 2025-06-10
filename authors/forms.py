7a534a7e4c47642911d7c52ef684a6f566d35162
Gabriel Andrei Junges <gabriel.junges@aprtech.com.br>
2025-06-10 09:32:44 -0300

    Alterado formulário de criação de receitas e criado validação de confirmação de senha.

--- authors/forms.py
+++ authors/forms.py
@@ -1,10 +1,57 @@
-from dataclasses import fields
-from pyexpat import model
+from django.core.exceptions import ValidationError
 from django import forms
 from django.contrib.auth.models import User
 
 
+def add_attr(field, attr_name, attr_new_val):
+    existing_attr = field.widget.attrs.get(attr_name, '')
+    field.widget.attr[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
+
+
+def add_placeholder(field, placeholder_val):
+    add_attr(field, 'placeholder', placeholder_val)
+
+
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Type your username')
+        add_placeholder(self.fields['email'], 'Type your e-mail')
+        add_placeholder(self.fields['first_name'],
+                        'Type your first name Ex.: John')
+        add_placeholder(self.fields['last_name'],
+                        'Type your last name Ex.: Doe')
+        add_attr(self.fields['username'], 'css', 'a-css-class')
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Type your password'
+        }),
+        error_messages={
+            'required': 'Password must not be empty'
+        },
+        help_text=(
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8'
+        )
+    )
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password'
+        }),
+        error_messages={
+            'required': 'Password must not be empty'
+        },
+        help_text=(
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8'
+        )
+    )
+
     class Meta:
         model = User
         fields = [
@@ -12,4 +59,49 @@ class RegisterForm(forms.ModelForm):
             'last_name',
             'username',
             'email',
-            'password']
+            'password',
+        ]
+
+        labels = {
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'username': 'Username',
+            'email': 'Email',
+            'password': 'Password',
+        }
+
+        help_texts = {
+            'email': 'The e-mail must be valid'
+        }
+
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty.',
+            }
+        }
+
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Type your username',
+                'class': 'input text-input'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password'
+            })
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        if password != password2:
+            password_confirmation_error = ValidationError(
+                'Password and password2 must be equal',
+                code='invalid'
+            )
+            raise ValidationError({
+                'password': password_confirmation_error,
+                'password2': [password_confirmation_error,],
+            })
--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from authors.forms import RegisterForm
 
@@ -7,10 +7,8 @@ from authors.forms import RegisterForm
 
 
 def register_view(request):
-    if request.POST:
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
     })
@@ -19,8 +17,9 @@ def register_view(request):
 def register_create(request):
     if not request.POST:
         raise Http404()
-    else:
-        form = RegisterForm(request.POST)
-    return render(request, 'authors/pages/register_view.html', {
-        'form': form,
-    })
+
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)
+
+    return redirect('authors:register')
